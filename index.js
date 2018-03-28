486be6b84e5f8a74449d5870b24b7d5a568594fd
Lior Rabin <lior@colu.com>
2018-03-28 17:18:29 +0300

    init function should return a promise

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const path = require('path')
 
-const make = function (dependenciesGraph, folderStruct) {
-  // nothing to do here
-}
-
 const init = function (config) {
-  const logger = require(path.join(__dirname, '/lib/winston'))(config)
-  return logger
+  return new Promise((resolve, reject) => {
+    const logger = require(path.join(__dirname, '/lib/winston'))(config)
+    resolve(logger)
+  })
 }
 
 module.exports = {
-  make: make,
   init: init
 }
