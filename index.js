62b1ffb77de032b0d1d13c4c0fe26f173a88e7f6
Tal Beja <tal@colu.com>
2018-04-09 17:15:35 +0300

    osseus instead of config resolve this and add winston to logger

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const path = require('path')
 
-const init = function (config) {
+const init = function (osseus) {
+  this.osseus = osseus
+  const config = osseus.config
   return new Promise((resolve, reject) => {
     config.get = function (key) {
       return config.osseus_logger[key.toLowerCase()] || config[key.toLowerCase()]
     }
-    const logger = require(path.join(__dirname, '/lib/winston'))(config)
-    resolve(logger)
+    const winston = require(path.join(__dirname, '/lib/winston'))(config)
+    this.winston = winston
+    resolve(this)
   })
 }
 
