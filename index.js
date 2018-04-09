fdafc7a354876b0c72b533c0a5a5df44069f2f0d
Lior Rabin <lior@colu.com>
2018-04-09 14:52:33 +0300

    osseus-config --> keep "osseus" prefix on top level keys

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const init = function (config) {
   return new Promise((resolve, reject) => {
     config.get = function (key) {
-      return config.logger[key.toLowerCase()] || config[key.toLowerCase()]
+      return config.osseus_logger[key.toLowerCase()] || config[key.toLowerCase()]
     }
     const logger = require(path.join(__dirname, '/lib/winston'))(config)
     resolve(logger)
