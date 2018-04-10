2f8a5a7a454de125bbb3babee21e9bbd77507314
Lior Rabin <lior@colu.com>
2018-04-10 14:05:13 +0300

    remove config.get

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const path = require('path')
 
 const init = function (osseus) {
   this.osseus = osseus
-  const config = osseus.config
   return new Promise((resolve, reject) => {
-    config.get = function (key) {
-      return config.osseus_logger[key.toLowerCase()] || config[key.toLowerCase()]
-    }
-    const logger = require(path.join(__dirname, '/lib/winston'))(config)
+    const logger = require(path.join(__dirname, '/lib/winston'))(osseus.config)
     Object.assign(this, logger)
     resolve(this)
   })
