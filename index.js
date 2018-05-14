b27d1c7cb40f09fcff3a9153365115be0ae9a397
Lior Rabin <lior@colu.com>
2018-05-14 11:50:02 +0300

    not needed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const path = require('path')
 
 const init = function (osseus) {
-  this.osseus = osseus
   return new Promise((resolve, reject) => {
     const logger = require(path.join(__dirname, '/lib/winston'))(osseus.config)
     Object.assign(this, logger)
