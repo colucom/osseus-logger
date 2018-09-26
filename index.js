9c6e43812cc777ed4ada33cf0d60a15f1be7aaba
nllahat <nllahat@users.noreply.github.com>
2018-09-26 17:12:27 +0300

    get aws instance id (#3)
    * get aws instance id
    
    * added aws module that uses the aws-sdk

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const path = require('path')
+const aws = require('./lib/aws')()
 
 const init = function (osseus) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async resolve => {
     const logger = require(path.join(__dirname, '/lib/winston'))(osseus.config)
+    const {name, pid} = await aws.getInstanceId()
+
+    logger.instanceId = name
+    logger.pid = pid
+
     Object.assign(this, logger)
     resolve(this)
   })
