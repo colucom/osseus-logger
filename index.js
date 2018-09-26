317a6b97c2d1295bfc52159ac266724bc418c966
netali <netali@colus-MacBook-Pro.local>
2018-09-26 15:15:41 +0300

    get aws instance id

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const path = require('path')
 
+function getAWSInstanceId () {
+  return new Promise(resolve => {
+    const metadata = require('node-ec2-metadata')
+    const os = require('os')
+
+    metadata.isEC2()
+      .then(onEC2 => {
+        if (!onEC2) {
+          return Promise.resolve(os.hostname())
+        }
+
+        return metadata.getMetadataForInstance('instance-id')
+      })
+      .then(resolve)
+      .catch(error => {
+        console.error(`ec2 getMetadataForInstance encountered an error: ${error}`)
+        resolve(os.hostname())
+      })
+  })
+}
+
 const init = function (osseus) {
-  return new Promise((resolve, reject) => {
+  return new Promise(async resolve => {
     const logger = require(path.join(__dirname, '/lib/winston'))(osseus.config)
+    logger.awsInstanceId = await getAWSInstanceId()
+
     Object.assign(this, logger)
     resolve(this)
   })
