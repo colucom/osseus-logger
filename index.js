95f555b21a7770e34d1c10c4d843f0cc8b6fd676
netali <netali@colus-MacBook-Pro.local>
2018-09-26 17:04:03 +0300

    added aws module that uses the aws-sdk

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 const path = require('path')
-
-function getAWSInstanceId () {
-  return new Promise(resolve => {
-    const metadata = require('node-ec2-metadata')
-    const os = require('os')
-
-    metadata.isEC2()
-      .then(onEC2 => {
-        if (!onEC2) {
-          return Promise.resolve(os.hostname())
-        }
-
-        return metadata.getMetadataForInstance('instance-id')
-      })
-      .then(resolve)
-      .catch(error => {
-        console.error(`ec2 getMetadataForInstance encountered an error: ${error}`)
-        resolve(os.hostname())
-      })
-  })
-}
+const aws = require('./lib/aws')()
 
 const init = function (osseus) {
   return new Promise(async resolve => {
     const logger = require(path.join(__dirname, '/lib/winston'))(osseus.config)
-    logger.awsInstanceId = await getAWSInstanceId()
+    const {name, pid} = await aws.getInstanceId()
+
+    logger.instanceId = name
+    logger.pid = pid
 
     Object.assign(this, logger)
     resolve(this)
